87dedea87ee11d58a87d46e9b68eee54c68195ce
Matus Kral <matuskral@me.com>
2014-02-15 11:09:44 +0100

    better handling of arm arch

--- gen.package.sh
+++ gen.package.sh
@@ -10,7 +10,7 @@ str='strip'
 strargs='--strip-unneeded'
 tot=0
 
-if [ "$(uname -m)" != 'armv6l' ]; then
+if ! dpkg-architecture -iarmhf; then
     arm-linux-gnueabihf-strip > /dev/null 2>&1
     [ $? -eq '127' ] && { echo "please install binutils-arm-linux-gnueabihf"; str=''; true; } || str='arm-linux-gnueabihf-strip'
 fi
