da9c9d9782aefad43e5961a5a902351cc73e98a6
Matus Kral <matuskral@me.com>
2014-05-15 07:17:23 +0200

    consolidated code imx/rpi fixed some initscripts conflicts for Debian/Jessie updated update-rc.d script to correctly allow pre linking from /etc/init.d to /lib/init/upstart-job

--- content/etc/profile.d/xbian-locale.sh
+++ content/etc/profile.d/xbian-locale.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
-[ -e /etc/default/locale ] && . /etc/default/locale
+if [ -z "$LANG" ]; then
+    [ -e /etc/default/locale ] && . /etc/default/locale
+    export LANG=$LANG
+fi
+
 export LC_CTYPE=C
 [ -n "$LANG" ] && export LC_ALL=$LANG
+
