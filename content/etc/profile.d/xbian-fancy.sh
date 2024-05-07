cb1544ae2d674eb6d4595a0dd71fd5583da73935
mkreisl <manfred.kreisl@nexgo.de>
2024-05-07 18:50:51 +0200

    Profile script xbian-fancy.sh works now under dash

--- content/etc/profile.d/xbian-fancy.sh
+++ content/etc/profile.d/xbian-fancy.sh
@@ -13,14 +13,14 @@ esac
 force_color_prompt=yes
 
 if [ -n "$force_color_prompt" ]; then
-    if [ -x /usr/bin/tput ] && tput setaf 1 >&/dev/null; then
-  # We have color support; assume it's compliant with Ecma-48
-  # (ISO/IEC-6429). (Lack of such support is extremely rare, and such
-  # a case would tend to support setf rather than setaf.)
-  color_prompt=yes
-    else
-  color_prompt=
-    fi
+   if [ -x /usr/bin/tput ] && tput setaf 1 >/dev/null 2>&1; then
+      # We have color support; assume it's compliant with Ecma-48
+      # (ISO/IEC-6429). (Lack of such support is extremely rare, and such
+      # a case would tend to support setf rather than setaf.)
+      color_prompt=yes
+   else
+      color_prompt=
+  fi
 fi
 
 if [ "$color_prompt" = yes ]; then
